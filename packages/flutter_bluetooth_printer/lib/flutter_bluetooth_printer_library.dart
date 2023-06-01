a6a89188f2a1e365940bd7ada2735f3a83bc4fa5
Alexandre <andriatto@hotmail.com>
2023-06-01 16:06:11 -0300

    update

--- packages/flutter_bluetooth_printer/lib/flutter_bluetooth_printer_library.dart
+++ packages/flutter_bluetooth_printer/lib/flutter_bluetooth_printer_library.dart
@@ -9,7 +9,7 @@ import 'package:flutter/rendering.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_bluetooth_printer/flutter_bluetooth_printer.dart';
 import 'package:flutter_bluetooth_printer_platform_interface/flutter_bluetooth_printer_platform_interface.dart';
-import 'package:image/image.dart' as img;
+import 'package:image_v3/image_v3.dart' as img;
 
 part 'src/errors/busy_device_exception.dart';
 part 'src/flutter_bluetooth_printer_impl.dart';
